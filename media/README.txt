6fd587dd6852803ba18da948d757441018329682
gtranquilli <55699994+gtranquilli@users.noreply.github.com>
2025-08-15 11:10:43 +0200

    Add files via upload

---
+++ media/README.txt
@@ -0,0 +1,2 @@
+Metti qui immagini o video per i tuoi esercizi.
+Usa mediaUrl in index.html per collegarli.
--- sw.js
+++ sw.js
@@ -1,28 +1,27 @@
-const CACHE = 'timer-pwa-v1';
+
+const CACHE = 'timer-pwa-v3-voice-media';
 const ASSETS = [
   './',
   './index.html',
   './manifest.webmanifest',
   './sw.js',
   './icons/icon-192.png',
-  './icons/icon-512.png'
+  './icons/icon-512.png',
+  './media/placeholder.png'
 ];
-
 self.addEventListener('install', (e) => {
   e.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
   self.skipWaiting();
 });
-
 self.addEventListener('activate', (e) => {
   e.waitUntil(
     caches.keys().then(keys => Promise.all(keys.map(k => (k !== CACHE) ? caches.delete(k) : null)))
   );
   self.clients.claim();
 });
-
 self.addEventListener('fetch', (e) => {
   const url = new URL(e.request.url);
-  if (url.origin === location.origin) {
+  if (e.request.method === 'GET' && url.origin === location.origin) {
     e.respondWith(
       caches.match(e.request).then(resp => resp || fetch(e.request))
     );
